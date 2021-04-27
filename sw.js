b8240e957a9855785dbc988b5bad875ff926ff3d
rickmrijs <83250011+rickmrijs@users.noreply.github.com>
2021-04-26 19:20:39 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Espera 11 minutos después de hacer los cambios en tu sitio, para depués
  * actualizar este archivo. */
-const CACHE = "rickmrijs-1.00";
+const CACHE = "rickmrijs-1.01";
 
 /** Archivos requeridos para que la aplicación funcione fuera de línea. */
 const ARCHIVOS = [
